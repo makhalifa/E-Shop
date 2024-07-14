f281862ef7c88244fc25ec390cdc9668d234710b
Mohamed Khalifa <7mdiano@gmail.com>
2024-07-14 16:37:33 +0300

    Add image upload and resize functionality to category service

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,6 +1,26 @@
-// Description: Service for category routes.
-const Category = require('../models/categoryModel');
 const Factory = require('./handlersFactory');
+const sharp = require('sharp');
+const AsyncHandler = require('express-async-handler');
+const { uploadSingleImage } = require('../middlewares/uploadImageMiddleware');
+const Category = require('../models/categoryModel');
+
+exports.uploadCategoryImage = uploadSingleImage('image');
+
+exports.resizeImage = AsyncHandler(async (req, res, next) => {
+  if (!req.file) return next();
+
+  const filename = `category-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(600, 600)
+    .toFormat('jpeg')
+    .jpeg({ quality: 95 })
+    .toFile(`uploads/categories/${filename}`);
+
+  req.body.image = filename;
+
+  next();
+});
 
 // @desc    Get all categories
 // @route   GET /api/v1/categories
