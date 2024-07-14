ce66bcfd8500e4625a3f8d6758a7365eeb234680
Mohamed Khalifa <7mdiano@gmail.com>
2024-07-14 16:40:06 +0300

    Add createdAt and updatedAt fields to brandModel schema

--- models/brandModel.js
+++ models/brandModel.js
@@ -20,7 +20,7 @@ const brandSchema = new mongoose.Schema(
         'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg',
     },
   },
-  { timestamps: true }
+  { timestamps: true } // timestamps: true will create createdAt and updatedAt fields
 );
 
 // 2- Create Model
