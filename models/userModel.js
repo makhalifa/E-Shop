42920eed7209b3000100485a6df80f20669820b0
Mohamed Khalifa <7mdiano@gmail.com>
2024-07-16 14:51:06 +0300

    Add user model with name, email, password, role, and active fields
    This commit adds a new user model with required fields for name, email, and password. It also includes optional fields for phone, profile image, role, and active status. The model includes pre and post hooks to set the profile image URL and timestamps.

---
+++ models/userModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+    },
+    phone: String,
+    profileImg: String,
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      min: [6, 'Password must be at least 6 characters'],
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const setImgUrl = (doc) => {
+  if (doc.profileImg) {
+    const imgUrl = `${process.env.BASE_URL}/users/${doc.profileImg}`;
+    doc.profileImg = imgUrl;
+  }
+};
+
+userSchema.post('init', (doc) => {
+  setImgUrl(doc);
+});
+
+userSchema.post('save', (doc) => {
+  setImgUrl(doc);
+});
+
+module.exports = mongoose.model('User', userSchema);
