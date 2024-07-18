b4ef9eda5b6b9ca99c957dff0ff7a10fa483a3a4
Mohamed Khalifa <7mdiano@gmail.com>
2024-07-18 11:57:30 +0300

    Add error handling for JWT token signature and expiration
    This commit adds two new functions to handle errors related to JWT token
    signature and expiration. The functions create a new instance of the
    ApiError class with the appropriate status code and message. The globalError
    middleware has been updated to check for these errors and call the new
    functions accordingly.

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,3 +1,5 @@
+const ApiError = require('../utils/apiError');
+
 const sendErrorDev = (err, res) =>
   res.status(err.statusCode).json({
     status: err.status,
@@ -12,13 +14,22 @@ const sendErrorProd = (err, res) =>
     message: err.message,
   });
 
-const globalError = (err, _req, res, _next) => { 
+const handleJwtInvalidSignature = () =>
+  new ApiError(401, 'Invalid token, please login again');
+
+const handleJwtExpiredSignature = () =>
+  new ApiError(401, 'Your token has expired, please login again');
+
+const globalError = (err, _req, res, _next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else {
+    if (err.name === 'JsonWebTokenError') err = handleJwtInvalidSignature();
+    else if (err.name === 'TokenExpiredError')
+      err = handleJwtExpiredSignature();
     sendErrorProd(err, res);
   }
 };
