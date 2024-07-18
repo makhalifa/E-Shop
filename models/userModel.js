cb4edb35ceee48bede1205df024a1c46b4c69c21
Mohamed Khalifa <7mdiano@gmail.com>
2024-07-18 12:33:23 +0300

    Update user role enum options in userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema(
     passwordChangedAt: Date,
     role: {
       type: String,
-      enum: ['user', 'admin'],
+      enum: ['user', 'moderator', 'admin'],
       default: 'user',
     },
     active: {
