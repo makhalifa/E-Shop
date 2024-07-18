053f6a9e27832c4897d5c2f33d1f7cb695adff28
Mohamed Khalifa <7mdiano@gmail.com>
2024-07-18 11:35:11 +0300

    Refactor categoryModel.js to use a default image and set image URL in post hooks
    The commit refactors the categoryModel.js file to use a default image and sets the image URL in post hooks for 'init' and 'save' events. The setImageUrl function logs the image and document objects for debugging purposes.

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -17,7 +17,7 @@ const categorySchema = new mongoose.Schema(
     image: {
       type: String,
       default:
-        'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg',
+        'image.png',
     },
   },
   { timestamps: true }
@@ -26,17 +26,20 @@ const categorySchema = new mongoose.Schema(
 const setImageUrl = (doc) => {
   // return image base url + image filename
   if (doc.image) {
+    console.log(doc.image);
     const imageUrl = `${process.env.BASE_URL}/categories/${doc.image}`;
     doc.image = imageUrl;
   }
+  console.log(doc);
+  return doc;
 };
 
 categorySchema.post('init', (doc) => {
-  setImageUrl(doc)
+  setImageUrl(doc);
 });
 
 categorySchema.post('save', (doc) => {
-  setImageUrl(doc)
+  setImageUrl(doc);
 });
 
 // 2- Create Model
