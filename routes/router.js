ce08f0546f5461ef4c873032dee86145acdd396e
Mohamed Khalifa <7mdiano@gmail.com>
2024-07-18 11:34:53 +0300

    Add router file with routes for different resources

---
+++ routes/router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+// Define router
+const router = express.Router();
+
+// Mount routes
+const brandRoute = require('./brandRoute');
+const categoryRoute = require('./categoryRoute');
+const subCategoryRoute = require('./subCategoryRoute');
+const productRoute = require('./productRoute');
+const userRoute = require('./userRoute');
+const authRoute = require('./authRoute');
+
+// Define routes for different resources
+router.use('/brands', brandRoute);
+router.use('/categories', categoryRoute);
+router.use('/subcategories', subCategoryRoute);
+router.use('/products', productRoute);
+router.use('/users', userRoute);
+router.use('/auth', authRoute);
+
+
+module.exports = router;
