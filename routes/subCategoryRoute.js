d87f600a35705db6dd9cf0ce81717907af754e17
Mohamed Khalifa <7mdiano@gmail.com>
2024-08-10 12:45:26 +0300

    Add authentication and authorization to subCategory routes
    - Import authService in subCategoryRoute.js
    - Protect GET, PUT, and DELETE routes with authService.protect
    - Restrict access to certain roles using authService.allowedTo for GET, PUT, and DELETE routes

--- routes/subCategoryRoute.js
+++ routes/subCategoryRoute.js
@@ -16,6 +16,8 @@ const {
   deleteSubCategoryValidator,
 } = require('../utils/validators/subCategoryValidator');
 
+const authService = require('../services/authService');
+
 // mergeParams: true is used to merge the params from the parent router
 // ex : /api/v1/categories/:categoryId/subcategories
 const router = express.Router({ mergeParams: true });
@@ -23,12 +25,27 @@ const router = express.Router({ mergeParams: true });
 router
   .route('/')
   .post(setCategoryIdToBody, createSubCategoryValidator, createSubCategory)
-  .get(createFilterObj, getSubCategories);
+  .get(
+    authService.protect,
+    authService.allowedTo('admin', 'moderator'),
+    createFilterObj,
+    getSubCategories
+  );
 
 router
   .route('/:id')
   .get(getSubCategoryValidator, getSubCategory)
-  .put(updateSubCategoryValidator, updateSubCategory)
-  .delete(deleteSubCategoryValidator, deleteSubCategory);
+  .put(
+    authService.protect,
+    authService.allowedTo('admin', 'moderator'),
+    updateSubCategoryValidator,
+    updateSubCategory
+  )
+  .delete(
+    authService.protect,
+    authService.allowedTo('admin'),
+    deleteSubCategoryValidator,
+    deleteSubCategory
+  );
 
 module.exports = router;
