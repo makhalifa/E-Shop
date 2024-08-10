8128ff8be287427e551227ae578f0b6e45016635
Mohamed Khalifa <7mdiano@gmail.com>
2024-08-10 12:37:48 +0300

    Add password reset functionality to auth routes
    - Added routes for forgot password, verify reset code, and reset password.
    - Included corresponding validators for each new route.

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,13 +3,29 @@ const express = require('express');
 // Define router
 const router = express.Router();
 
-const { signup, login } = require('../services/authService');
+const {
+  signup,
+  login,
+  forgotPassword,
+  verifyPasswordResetCode,
+  resetPassword,
+} = require('../services/authService');
 const {
   signupValidator,
   loginValidator,
+  forgetPasswordValidator,
+  verifyPasswordResetCodeValidator,
+  resetPasswordValidator,
 } = require('../utils/validators/authValidator');
 
 router.post('/signup', signupValidator, signup);
 router.post('/login', loginValidator, login);
+router.post('/forgot-password', forgetPasswordValidator, forgotPassword);
+router.post(
+  '/verify-reset-code',
+  verifyPasswordResetCodeValidator,
+  verifyPasswordResetCode
+);
+router.put('/reset-password', resetPasswordValidator, resetPassword);
 
 module.exports = router;
