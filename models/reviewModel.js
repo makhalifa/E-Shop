1db5172813f0a3fb4abae504f828e8b5ad301d29
Mohamed Khalifa <7mdiano@gmail.com>
2024-08-11 14:36:30 +0300

    Add review model with schema for title, rating, user, and product in Mongoose

---
+++ models/reviewModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 100,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user'],
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: [true, 'Review must belong to a product'],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Review', reviewSchema);
