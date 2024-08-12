068867897f424d26dcaa3e91cc54085061c51ad3
Mohamed Khalifa <7mdiano@gmail.com>
2024-08-12 14:12:42 +0300

    Add wishlist management endpoints: remove product and get user wishlist

--- services/wishlistService.js
+++ services/wishlistService.js
@@ -1,10 +1,9 @@
 const AsyncHandler = require('express-async-handler');
-const ApiError = require('../utils/apiError');
 
 const User = require('../models/userModel');
 
-// @desc    Add Product to Wishlists
-// @route   POST /api/v1/whishlists
+// @desc    Add Product to Wishlist
+// @route   POST /api/v1/wishlist
 // @access  Private - User
 exports.addToWishlist = AsyncHandler(async (req, res, next) => {
   const user = await User.findByIdAndUpdate(
@@ -18,4 +17,33 @@ exports.addToWishlist = AsyncHandler(async (req, res, next) => {
     message: 'Product added to wishlist',
     data: user.wishlist,
   });
+});
+
+// @desc    Remove Product from Wishlist
+// @route   DELETE /api/v1/wishlist
+// @access  Private - User
+exports.removeFromWishlist = AsyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    { $pull: { wishlist: req.params.productId } },
+    { new: true }
+  ).exec();
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Product removed from wishlist',
+    data: user.wishlist,
+  });
+});
+
+// @desc    Get logged User wishlist
+// @route   GET /api/v1/wishlist
+// @access  Private - User
+exports.getLoggedUserWishlist = AsyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user._id).populate({
+    path: 'wishlist',
+    select: 'name slug price',
+  });
+
+  res.status(200).json({ data: user.wishlist });
 });
