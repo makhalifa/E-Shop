138f9e612224ee53d64fdda2138475fe63f2e6a4
Mohamed Khalifa <7mdiano@gmail.com>
2024-08-12 13:58:49 +0300

    Add wishlist validation middleware to check product ID validity

---
+++ utils/validators/wishlistValidator.js
@@ -0,0 +1,19 @@
+const { check } = require('express-validator');
+
+const validationMiddleware = require('../../middlewares/validatorMiddleware');
+const Product = require('../../models/productModel');
+
+exports.addToWishlistValidator = [
+  check('productId')
+    .isMongoId()
+    .withMessage('Invalid ID')
+    .custom((value, { req }) =>
+      // check if productid is exist
+      Product.findById(value).then((product) => {
+        if (!product) {
+          return Promise.reject(new Error('Invalid ID'));
+        }
+      })
+    ),
+  validationMiddleware,
+];
