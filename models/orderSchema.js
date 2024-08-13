1b812cbc1e7ff44cd6ee95c02604ffbeab123c32
Mohamed Khalifa <7mdiano@gmail.com>
2024-08-13 17:21:09 +0300

    Add order schema model with user, order items, and payment details

---
+++ models/orderSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+
+    orderItems: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        quantity: { type: Number, required: true }, // quantity
+        color: { type: String, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+
+    paymentMethod: { type: String, enum: ['online', 'cash'], default: 'cash' },
+
+    taxPrice: { type: Number, default: 0.0 },
+    shippingPrice: { type: Number, default: 0.0 },
+    totalPrice: { type: Number, required: true },
+
+    isPaid: { type: Boolean, default: false },
+    paidAt: { type: Date },
+
+    isDelivered: { type: Boolean, default: false },
+    deliveredAt: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Order', orderSchema);
