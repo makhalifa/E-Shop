f930ec32d49715c054dda7be88b342a01189a52b
Mohamed Khalifa <7mdiano@gmail.com>
2024-08-16 09:15:21 +0300

    Add shippingAddress field and pre-find hook to orderModel
    - Introduced `shippingAddress` field to store address details, phone, city, and postal code.
    - Added a pre-find hook to populate user and orderItems.product fields with relevant details.

--- models/orderModel.js
+++ models/orderModel.js
@@ -21,6 +21,13 @@ const orderSchema = mongoose.Schema(
       },
     ],
 
+    shippingAddress: {
+      details: String,
+      phone: String,
+      city: String,
+      postalCode: String,
+    },
+
     paymentMethod: { type: String, enum: ['online', 'cash'], default: 'cash' },
 
     taxPrice: { type: Number, default: 0.0 },
@@ -37,5 +44,16 @@ const orderSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+orderSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'user',
+    select: 'name email profileImg phone',
+  }).populate({
+    path: 'orderItems.product',
+    select: 'title imageCover',
+  });
+  next();
+});
 
 module.exports = mongoose.model('Order', orderSchema);
